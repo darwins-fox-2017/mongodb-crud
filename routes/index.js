49913d3f5e65de3e4a22d126adb84478078e3fcc
didietsuryadi <didietsuryadi>
2017-03-01 15:42:59 +0700

    create-routing

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,34 @@
 var express = require('express');
 var router = express.Router();
+var bookController = require('../controllers/bookController')
+var customerController = require('../controllers/customerController')
+var transactionController = require('../controllers/transactionController')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+//routing books
+router.get('/api/book/:id', bookController.readBook)
+router.get('/api/book', bookController.readBooks)
+router.post('/api/book/:id', bookController.createBook)
+router.put('/api/book/:id', bookController.updateBook)
+router.delete('/api/book/:id', bookController.deleteBook)
+
+//routing customer
+router.get('/api/customer/:id', customerController.readCustomer)
+router.get('/api/customer', customerController.readCustomers)
+router.post('/api/customer/:id', customerController.createCustomer)
+router.put('/api/customer/:id', customerController.updateCustomer)
+router.delete('/api/customer/:id', customerController.deleteCustomer)
+
+//routing transaction
+router.get('/api/transaction/:id', transactionController.readTransaction)
+router.get('/api/transaction', transactionController.readTransactions)
+router.post('/api/transaction/:id', transactionController.createTransaction)
+router.put('/api/transaction/:id', transactionController.updateTransaction)
+router.delete('/api/transaction/:id', transactionController.deleteTransaction)
+
 
 module.exports = router;
